309176456b777dcb2d55b45ad29236a5ab64cbab
exploitenomah <iksploiting@gmail.com>
2023-07-24 02:28:57 +0100

    create links assets

---
+++ assets/links.ts
@@ -0,0 +1,22 @@
+export const socials = [
+  {
+    name: "twitter",
+    icon: "/images/twitter.svg",
+    link: "https://twitter.com/Queen_Exploit",
+  },
+  {
+    name: "github",
+    icon: "/images/github.svg",
+    link: "https://github.com/exploitenomah",
+  },
+  {
+    name: "linkedin",
+    icon: "/images/linkedin.svg",
+    link: "https://www.linkedin.com/in/exploit-enomah-b62289225/",
+  },
+  {
+    name: "instagram",
+    icon: "/images/instagram.svg",
+    link: "https://www.instagram.com/exploit_enomah/",
+  }
+]
