764322812f42552723bc719d48c67e9ef88c11ce
exploitenomah <iksploiting@gmail.com>
2023-07-24 02:25:18 +0100

    create app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import Layout from "../components/Layout"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import useRemoveElements from "../hooks/useRemoveElements"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  const removeElements = useRemoveElements()
+
+  useEffect(() => {
+    if (router.asPath !== "/") removeElements(".bubble", document.body)
+  }, [removeElements, router.asPath])
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
